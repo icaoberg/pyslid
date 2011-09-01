32e878af9e8d29ead4c7ce3788c79efe2b409c65
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2011-09-01 17:01:34 -0400

    Fixed a bug in clink where features where not calculated due to missing input arguments

--- pslid/features.py
+++ pslid/features.py
@@ -29,7 +29,7 @@ send email to murphy@cmu.edu
 import omero, pyslic, pslid.utilities
 import omero.callbacks
 
-def calculate( session, iid, set="slf34", field=True, rid=[], pixels=0, zslice=0, timepoint=0, threshold=None ):
+def calculate( session, iid, set="slf34", field=True, rid=None, pixels=0, zslice=0, timepoint=0, threshold=None ):
     """
     Calculates and returns a feature ids and features vectors given a valid
     image identification (iid). It currently calculates SLF33, SLF34, SLF35 and SLF36.
@@ -48,10 +48,13 @@ def calculate( session, iid, set="slf34", field=True, rid=[], pixels=0, zslice=0
         #get image information from omero
         image = pslid.utilities.getImage(session, iid)
         try:
+            if threshold == None:
+                threshold = 10*1024;
+
             #check if image size is greater than threshold value
-            if img.getPixels(pixels).getSizeX().getValue() > threshold:
+            if image.getPixels(pixels).getSizeX().getValue() > threshold:
                 return [[],[]]
-            elif img.getPixels(pixels).getSizeY().getValue() > threshold:
+            elif image.getPixels(pixels).getSizeY().getValue() > threshold:
                 return [[],[]]
             else:
                 #set scale value
@@ -63,7 +66,7 @@ def calculate( session, iid, set="slf34", field=True, rid=[], pixels=0, zslice=0
     #create gateway
     gateway = session.createGateway()
 
-    ids = ["SLF27.66","SLF27.67","SLF27.68","SLF27.69","SLF27.70","SLF27.71","SLF27.72","SLF27.73","SLF27.74","SLF27.75","SLF27.76","SLF27.77","SLF27.78","SLF33.37","SLF33.38","SLF33.39","SLF33.40","SLF33.41","SLF33.42","SLF33.43","SLF33.44","SLF33.45","SLF33.46","SLF33.47","SLF33.48","SLF33.49","SLF33.50","SLF33.51","SLF33.52","SLF33.53","SLF33.54","SLF33.55","SLF33.56","SLF33.57","SLF33.58","SLF33.59","SLF33.60","SLF33.61","SLF33.62","SLF33.63","SLF33.64","SLF33.65","SLF33.66","SLF33.67","SLF33.68","SLF33.69","SLF33.70","SLF33.71","SLF33.72","SLF33.73","SLF33.74","SLF33.75","SLF33.76","SLF33.77","SLF33.78","SLF33.79","SLF33.80","SLF33.81","SLF33.82","SLF33.83","SLF33.84","SLF33.85","SLF33.86","SLF33.87","SLF33.88","SLF33.89","SLF33.90","SLF33.91","SLF33.92","SLF33.93","SLF33.94","SLF33.95","SLF33.96","SLF33.97","SLF33.98","SLF33.99","SLF33.100","SLF33.101","SLF33.102","SLF33.103","SLF33.104","SLF33.105","SLF33.106","SLF33.107","SLF33.108","SLF33.109","SLF33.110","SLF33.111","SLF33.112","SLF33.113","SLF33.114","SLF27.1","SLF27.2","SLF27.3","SLF27.4","SLF27.5","SLF27.89","SLF27.90","SLF27.9","SLF27.10","SLF27.11","SLF27.12","SLF27.13","SLF27.80","SLF27.81","SLF27.82","SLF27.83","SLF27.84","SLF27.79","SLF31.1","SLF31.2","SLF31.3","SLF31.4","SLF31.5","SLF31.6","SLF31.7","SLF31.8","SLF31.9","SLF31.10","SLF31.11","SLF31.12","SLF31.13","SLF31.14","SLF31.15","SLF31.16","SLF31.17","SLF31.18","SLF33.1","SLF33.2","SLF33.3","SLF33.4","SLF33.5","SLF33.6","SLF33.7","SLF33.8","SLF33.9","SLF33.19","SLF33.20","SLF33.21","SLF33.22","SLF33.23","SLF33.24","SLF33.25","SLF33.26","SLF33.27","SLF33.10","SLF33.11","SLF33.12","SLF33.13","SLF33.14","SLF33.15","SLF33.16","SLF33.17","SLF33.18","SLF33.28","SLF33.29","SLF33.30","SLF33.31","SLF33.32","SLF33.33","SLF33.34","SLF33.35","SLF33.36","SLF34.1","SLF34.2","SLF34.3","SLF34.4","SLF34.5","SLF34.6","SLF34.7","SLF34.8","SLF34.9","SLF34.10","SLF27.80","SLF27.81","SLF27.82","SLF27.83","SLF27.84","SLF27.79","SLF27.1","SLF27.2","SLF27.3","SLF27.4","SLF27.5","SLF27.6","SLF27.7","SLF27.8","SLF27.85","SLF27.86","SLF27.87","SLF27.88","SLF27.89","SLF27.90","SLF27.14","SLF27.15","SLF27.16","SLF27.17","SLF27.18","SLF27.19","SLF27.20","SLF27.21","SLF27.22","SLF27.23","SLF27.24","SLF27.25","SLF27.26","SLF27.27","SLF27.28","SLF27.29","SLF27.30","SLF27.31","SLF27.32","SLF27.33","SLF27.34","SLF27.35","SLF27.36","SLF27.37","SLF27.38","SLF27.39","SLF27.40","SLF27.41","SLF27.42","SLF27.43","SLF27.44","SLF27.45","SLF27.46","SLF27.47","SLF27.48","SLF27.49","SLF27.50","SLF27.51","SLF27.52","SLF27.53","SLF27.54","SLF27.55","SLF27.56","SLF27.57","SLF27.58","SLF27.59","SLF27.60","SLF27.61","SLF27.62","SLF27.63","SLF27.64","SLF27.65","SLF27.66","SLF27.67","SLF27.68","SLF27.69","SLF27.70","SLF27.71","SLF27.72","SLF27.73","SLF27.74","SLF27.75","SLF27.76","SLF27.77","SLF27.78","SLF27.9","SLF27.10","SLF27.11","SLF27.12","SLF27.13","SLF31.1","SLF31.2","SLF31.3","SLF31.4","SLF31.5","SLF31.6","SLF31.7","SLF31.8","SLF31.9","SLF31.10","SLF31.11","SLF31.12","SLF31.13","SLF31.14","SLF31.15","SLF31.16","SLF31.17","SLF31.18"]
+    feature_ids = ["SLF27.66","SLF27.67","SLF27.68","SLF27.69","SLF27.70","SLF27.71","SLF27.72","SLF27.73","SLF27.74","SLF27.75","SLF27.76","SLF27.77","SLF27.78","SLF33.37","SLF33.38","SLF33.39","SLF33.40","SLF33.41","SLF33.42","SLF33.43","SLF33.44","SLF33.45","SLF33.46","SLF33.47","SLF33.48","SLF33.49","SLF33.50","SLF33.51","SLF33.52","SLF33.53","SLF33.54","SLF33.55","SLF33.56","SLF33.57","SLF33.58","SLF33.59","SLF33.60","SLF33.61","SLF33.62","SLF33.63","SLF33.64","SLF33.65","SLF33.66","SLF33.67","SLF33.68","SLF33.69","SLF33.70","SLF33.71","SLF33.72","SLF33.73","SLF33.74","SLF33.75","SLF33.76","SLF33.77","SLF33.78","SLF33.79","SLF33.80","SLF33.81","SLF33.82","SLF33.83","SLF33.84","SLF33.85","SLF33.86","SLF33.87","SLF33.88","SLF33.89","SLF33.90","SLF33.91","SLF33.92","SLF33.93","SLF33.94","SLF33.95","SLF33.96","SLF33.97","SLF33.98","SLF33.99","SLF33.100","SLF33.101","SLF33.102","SLF33.103","SLF33.104","SLF33.105","SLF33.106","SLF33.107","SLF33.108","SLF33.109","SLF33.110","SLF33.111","SLF33.112","SLF33.113","SLF33.114","SLF27.1","SLF27.2","SLF27.3","SLF27.4","SLF27.5","SLF27.89","SLF27.90","SLF27.9","SLF27.10","SLF27.11","SLF27.12","SLF27.13","SLF27.80","SLF27.81","SLF27.82","SLF27.83","SLF27.84","SLF27.79","SLF31.1","SLF31.2","SLF31.3","SLF31.4","SLF31.5","SLF31.6","SLF31.7","SLF31.8","SLF31.9","SLF31.10","SLF31.11","SLF31.12","SLF31.13","SLF31.14","SLF31.15","SLF31.16","SLF31.17","SLF31.18","SLF33.1","SLF33.2","SLF33.3","SLF33.4","SLF33.5","SLF33.6","SLF33.7","SLF33.8","SLF33.9","SLF33.19","SLF33.20","SLF33.21","SLF33.22","SLF33.23","SLF33.24","SLF33.25","SLF33.26","SLF33.27","SLF33.10","SLF33.11","SLF33.12","SLF33.13","SLF33.14","SLF33.15","SLF33.16","SLF33.17","SLF33.18","SLF33.28","SLF33.29","SLF33.30","SLF33.31","SLF33.32","SLF33.33","SLF33.34","SLF33.35","SLF33.36","SLF34.1","SLF34.2","SLF34.3","SLF34.4","SLF34.5","SLF34.6","SLF34.7","SLF34.8","SLF34.9","SLF34.10","SLF27.80","SLF27.81","SLF27.82","SLF27.83","SLF27.84","SLF27.79","SLF27.1","SLF27.2","SLF27.3","SLF27.4","SLF27.5","SLF27.6","SLF27.7","SLF27.8","SLF27.85","SLF27.86","SLF27.87","SLF27.88","SLF27.89","SLF27.90","SLF27.14","SLF27.15","SLF27.16","SLF27.17","SLF27.18","SLF27.19","SLF27.20","SLF27.21","SLF27.22","SLF27.23","SLF27.24","SLF27.25","SLF27.26","SLF27.27","SLF27.28","SLF27.29","SLF27.30","SLF27.31","SLF27.32","SLF27.33","SLF27.34","SLF27.35","SLF27.36","SLF27.37","SLF27.38","SLF27.39","SLF27.40","SLF27.41","SLF27.42","SLF27.43","SLF27.44","SLF27.45","SLF27.46","SLF27.47","SLF27.48","SLF27.49","SLF27.50","SLF27.51","SLF27.52","SLF27.53","SLF27.54","SLF27.55","SLF27.56","SLF27.57","SLF27.58","SLF27.59","SLF27.60","SLF27.61","SLF27.62","SLF27.63","SLF27.64","SLF27.65","SLF27.66","SLF27.67","SLF27.68","SLF27.69","SLF27.70","SLF27.71","SLF27.72","SLF27.73","SLF27.74","SLF27.75","SLF27.76","SLF27.77","SLF27.78","SLF27.9","SLF27.10","SLF27.11","SLF27.12","SLF27.13","SLF31.1","SLF31.2","SLF31.3","SLF31.4","SLF31.5","SLF31.6","SLF31.7","SLF31.8","SLF31.9","SLF31.10","SLF31.11","SLF31.12","SLF31.13","SLF31.14","SLF31.15","SLF31.16","SLF31.17","SLF31.18"]
     
     if set=="slf34":
         #make pyslic image container
@@ -95,10 +98,14 @@ def calculate( session, iid, set="slf34", field=True, rid=[], pixels=0, zslice=0
             img.channels[c]=channel_num
             img.channeldata[c]=pslid.utilities.getPlane(session,iid,zslice,channel_num,timepoint)
         img.loaded=True
+        ids = []
         features = []    
 
+        indices=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163]
+        for i in range(len(indices)):
+            ids.append( feature_ids[indices[i]-1] )    
         features = pyslic.computefeatures(img,'field+')
-        return [ids[0:161], features]
+        return [ids, features]
     elif set=="slf36":
         #make pyslic image container
         img=pyslic.Image()
@@ -116,10 +123,10 @@ def calculate( session, iid, set="slf34", field=True, rid=[], pixels=0, zslice=0
         features = []
 
         values = pyslic.computefeatures(img,'field-dna+')
-        indexes = [12,10,11,1,18,17,6,7,8,9,20,2,3,19,4,5,21,22,13,14,15,16]
-        for i in range(len(indexes)):
-            ids.append( ids[indexes[i]-1] )
-            values.append( values[indexes[i]-1] )
+        indices = [12,10,11,1,18,17,6,7,8,9,20,2,3,19,4,5,21,22,13,14,15,16]
+        for i in range(len(indices)):
+            ids.append( feature_ids[indices[i]-1] )
+            values.append( values[indices[i]-1] )
         return [ids, values]
     elif set=="slf35":
         #make pyslic image container
@@ -138,17 +145,17 @@ def calculate( session, iid, set="slf34", field=True, rid=[], pixels=0, zslice=0
         features = []
 
         values = pyslic.computefeatures(img,'field-dna+')
-        indexes =[170,77,119,13,25,100,167,85,173,160,3,165,83,82,30,16,134,96,114,35,94,98,168]
-        for i in range(len(indexes)):
-            ids.append( ids[indexes[i]-1] )
-            features.append( values[indexes[i]-1] )
+        indices =[170,77,119,13,25,100,167,85,173,160,3,165,83,82,30,16,134,96,114,35,94,98,168]
+        for i in range(len(indices)):
+            ids.append( feature_ids[indices[i]-1] )
+            features.append( values[indices[i]-1] )
         return [ids, features]
     else:
         ids = []
         values = []
         return [ids, features]
 
-def link( session, iid, feature_ids, features, set, field=True, rid=[], overwrite=False ):
+def link( client, session, iid, feature_ids, features, set, field=True, rid=None, overwrite=False ):
     """
     Creates a table from the features vector and links
     the table to image with the given image id (iid). If the feature array is
@@ -168,17 +175,15 @@ def link( session, iid, feature_ids, features, set, field=True, rid=[], overwrit
     
     #check if image exist
     if not pslid.utilities.hasImage( session, iid ):
-        print "Image not found"
         return False
          
     #check if there is already a feature table attached to the image
     if pslid.features.has( session, iid, set, field ) and not overwrite:
-        print "Feature table already found"
         return False
 
     if overwrite:
-        pslid.features.unlink( session, iid, set, field )
-    
+        pslid.features.unlink( client, session, iid, set, field, rid )
+
     try:
         columns = []
         for i in range(len(features)):
@@ -276,7 +281,7 @@ def get( session, iid, set="slf34", field=True, rid=[], calculate=False, pixels=
     else:
         return [[],[]]
 
-def clink( client, session, iid, pixels=0, timepoint=0, set="slf34", field=True, rid=[], threshold=None, overwrite=False ):
+def clink( client, session, iid, set="slf34", field=True, rid=None, pixels=0, zslice=0, timepoint=0, threshold=None, overwrite=False ):
     """
     Calculates and links features to an object type given a valid id for the object.
     @param session
@@ -296,15 +301,15 @@ def clink( client, session, iid, pixels=0, timepoint=0, set="slf34", field=True,
         return False
             
     try:
-        [ids, features] = pslid.features.calculate( session, iid, set, pixels, zslice, timepoint, threshold )
+        [ids, features] = pslid.features.calculate( session, iid, set, field, rid, pixels, zslice, timepoint, threshold )
     except:
-        "Couldn't calculate features"
+        return False
 
     try:
-        answer = pslid.features.link( session, iid, ids, features, set, field, rid, overwrite )
+        answer = pslid.features.link( client, session, iid, ids, features, set, field, rid, overwrite )
         return answer
     except:
-        "Couldn't attach feature table to image"
+        return False
     
 def has( session, iid, set="slf34", field=True ):
     """
@@ -358,7 +363,7 @@ def has( session, iid, set="slf34", field=True ):
     else:
         return True
     
-def unlink( client, session, iid, set="slf34", field=True ):
+def unlink( client, session, iid, set="slf34", field=True, rid=None ):
     """
     Unlinks and removes feature tables. If more than one feature table with the same 
     name are found to be linked to the same image, this function removes them all.
@@ -402,7 +407,7 @@ def unlink( client, session, iid, set="slf34", field=True ):
 
     try:
         try:
-            cb.loop(10, 500)
+            callback.loop(10, 500)
         except omero.LockTimeout:
             print "Not finished in 5 seconds. Cancelling..."
 
