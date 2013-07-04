fdd44a2e666de9ba2655c275107971eeb99a95c8
Simon Li <spli@dundee.ac.uk>
2013-07-04 16:24:05 +0100

    Replace distutils with setuptools, specify install_requires

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from distutils.core import setup
+from setuptools import setup
+
 setup(name='pyslid',
       version='0.0.2',
       description='Protein Subcellular Location Image Database for OMERO',
@@ -7,7 +8,25 @@ setup(name='pyslid',
       maintainer='Robert F. Murphy',
       maintainer_email='murphy@cmu.edu',
       url='http://murphylab.web.cmu.edu/software/',
-      py_modules=['pyslid.features', 'pyslid.utilities', 'pyslid.database.link', \
-        'pyslid.image', 'pyslid.database.direct', 'pyslid.table'])
+      py_modules=[
+        'pyslid.features',
+        'pyslid.utilities',
+        'pyslid.database.link',
+        'pyslid.image',
+        'pyslid.database.direct',
+        'pyslid.table',
+        ],
+      install_requires = [
+        # pip install numpy and scipy just doesn't work, so make sure you
+        # manually install them first
+        'numpy>=1.4.1',
+        'scipy>=0.7.2',
+        # Note: mahotas requires the freeimage library
+        'mahotas==0.9.4',
+        'milk==0.4.3',
+        'pymorph==0.96',
+        'pyslic==0.6.1',
+        ],
+)
 
 
