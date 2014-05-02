0949df3f26d7f6f1dc32ff3b36e972b288e93c13
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2014-05-02 14:25:07 -0400

    Updated wrapper for making projects

--- pyslid/utilities.py
+++ pyslid/utilities.py
@@ -2,7 +2,7 @@
 Authors: Jennifer Bakal and Ivan E. Cao-Berg
 Created: May 1, 2011
 
-Copyright (C) 2011-2013 Murphy Lab
+Copyright (C) 2011-2014 Murphy Lab
 Lane Center for Computational Biology
 School of Computer Science
 Carnegie Mellon University
